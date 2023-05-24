1971ec64b4eaa36275b31b6f633b422c64f8a262
TouatiM199 <touatimanel27@gmail.com>
2023-05-24 16:31:43 +0100

    modifi ALLOWED_HOSTS

--- mon_projet/my_blog/my_blog/settings.py
+++ mon_projet/my_blog/my_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--e_k^cx&yjo9yy0@(j!^rvthcp8-xdbo4nu47!%9lds_*%0n_*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0','8.8.8.8']
+ALLOWED_HOSTS = ['*']
 
 
 
